92c77998df02ce8dfa656509a71181767d6be7e6
Jiayao Zhang <zjohnson@connect.hku.hk>
2018-04-28 00:51:23 +0800

    finalise bn

---
+++ code/bn.r
@@ -0,0 +1,7 @@
+library(bnlearn)
+gradcafe <- read.csv('cs_level.csv')
+df <- gradcafe[, c('degree', 'decision', 'decision_method',
+		   'gpa_level', 'gre_level', 'uni_pub_level', 'status')]
+hcdag <- hc(na.omit(df))
+mb(hcdag, 'decision')
+hcfit <- bn.fit(hcdag, na.omit(df))
